ffd23fcef0306b9dc992590ac22395293b8c53d1
StephanPatterson <stephan.patterson@lsus.edu>
2023-05-01 10:54:27 -0500

    Initial Commit

---
+++ ColGen_IPPrice_4d/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  ColGen_IPPrice_4d
+//
+//  Created by StephanP on 5/1/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+   // insert code here...
+   std::cout << "Hello, World!\n";
+   return 0;
+}
